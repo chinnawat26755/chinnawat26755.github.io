dbcf32956e49a111a32dd8b5920b26175215aedb
chinnawat26755 <chinnawat.aoum@bumail.net>
2024-11-21 01:42:55 +0700

    Update condb.php

--- config/condb.php
+++ config/condb.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = ""; // ถ้าไม่ได้ตั้งรหัสผ่านให้ลบ yourpassword ออก
-
+// PHP Data Objects(PDO) Sample Code:
 try {
-  $condb = new PDO("mysql:host=$servername;dbname=db_website;charset=utf8", $username, $password);
-  // กำหนดโหมดข้อผิดพลาดให้แสดงเป็น exception
-  $condb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    $conn = new PDO("sqlsrv:server = tcp:inventoryfanilpro.database.windows.net,1433; Database = inventory123", "projectsamsan", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "projectsamsan", "pwd" => "{your_password_here}", "Database" => "inventory123", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:inventoryfanilpro.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
