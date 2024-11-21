b0615c06094673ac2186b962b2d4d0d1fc21fd0f
chinnawat26755 <chinnawat.aoum@bumail.net>
2024-11-21 21:19:19 +0700

    Update condb.php

--- config/condb.php
+++ config/condb.php
@@ -1,16 +1,13 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:samsantech.database.windows.net,1433; Database = db_website", "fiw_cnw", "ChinnawatAoumkaew01");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+$host = 'tcp:samsantech.database.windows.net,1433';
+$dbName = 'db_website';
+$username = 'fiw_cnw';
+$password = 'ChinnawatAoumkaew01';
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "fiw_cnw", "pwd" => "ChinnawatAoumkaew01", "Database" => "db_website", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:samsantech.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+try {
+    $conn = new PDO("sqlsrv:server=$host;Database=$dbName", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 ?>
