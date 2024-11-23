8ded07b73c32087c8258dbfc26f30be129b5e930
chinnawat26755 <chinnawat.aoum@bumail.net>
2024-11-23 22:11:57 +0700

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,30 +1,22 @@
 <?php session_start(); ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Registration System PDO</title>
-  <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
   <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="assets/dist/css/adminlte.min.css?v=3.2.0">
 </head>
-
 <body class="hold-transition login-page">
   <div class="login-box">
     <div class="login-logo">
       <b>SamsunTech</b>System
     </div>
-    <!-- /.login-logo -->
     <div class="card">
       <div class="card-body login-card-body">
         <p class="login-box-msg">เข้าสู่ระบบเพื่อใช้งาน</p>
-
-        <!-- Display error or success messages -->
         <?php if(isset($_SESSION['error'])) { ?>
           <div class="alert alert-danger" role="alert">
             <?php 
@@ -33,16 +25,6 @@
             ?>
           </div>
         <?php } ?>
-        <?php if(isset($_SESSION['success'])) { ?>
-          <div class="alert alert-success" role="alert">
-            <?php 
-              echo $_SESSION['success'];
-              unset($_SESSION['success']);
-            ?>
-          </div>
-        <?php } ?>
-
-        <!-- Login Form -->
         <form action="signin_db.php" method="post">
           <div class="input-group mb-3">
             <input type="email" class="form-control" name="email" placeholder="Email">
@@ -66,16 +48,10 @@
             </div>
           </div>
         </form>
-
-        <div class="social-auth-links text-center mb-3">
-          <p>- หรือ -</p>
-        </div>
-
         <p class="mb-0">
           <a href="index.php" class="text-center">สมัครสมาชิก?</a>
         </p>
       </div>
-      <!-- /.login-card-body -->
     </div>
   </div>
 </body>
