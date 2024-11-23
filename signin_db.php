0b95b3cd8782d7742a45491fab3822bf8a9caf3a
chinnawat26755 <chinnawat.aoum@bumail.net>
2024-11-23 23:04:55 +0700

    Update signin_db.php

--- signin_db.php
+++ signin_db.php
@@ -42,7 +42,7 @@ if (isset($_POST['signin'])) {
                 }
             } else {
                 $_SESSION['error'] = "ไม่มีข้อมูลในระบบ";
-                header("location: signin.php"); // กลับไปหน้า signin
+                header("location: /mywebsite/admin/index.php"); // กลับไปหน้า signin
             }
         } catch(PDOException $e) {
             // แสดงข้อความ error หากเกิดปัญหากับฐานข้อมูล
