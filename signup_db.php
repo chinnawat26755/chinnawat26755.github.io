76449d079ae18adcf2d563ab98e97c6cf72bdede
chinnawat26755 <chinnawat.aoum@bumail.net>
2024-11-23 22:47:16 +0700

    Update signup_db.php

--- signup_db.php
+++ signup_db.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     session_start();
     require_once 'config/condb.php';
 
@@ -28,30 +28,38 @@
         } else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
             $_SESSION['error'] = 'รหัสผ่านต้องมีความยาวระหว่าง 5 ถึง 20 ตัวอักษร';
             header("location: index.php");
+        } else if (empty($c_password)) {
+            $_SESSION['error'] = 'กรุณายืนยันรหัสผ่าน';
+            header("location: index.php");
         } else if ($password != $c_password) {
             $_SESSION['error'] = 'รหัสผ่านไม่ตรงกัน';
             header("location: index.php");
         } else {
-            $password_hash = password_hash($password, PASSWORD_DEFAULT);
-
             try {
-                $check_data = $condb->prepare("SELECT * FROM users WHERE email = :email");
-                $check_data->bindParam(":email", $email);
-                $check_data->execute();
-                if ($check_data->rowCount() > 0) {
-                    $_SESSION['error'] = 'อีเมลนี้ถูกใช้งานไปแล้ว';
+                // ตรวจสอบชื่อจริงในระบบ
+                $check_name = $condb->prepare("SELECT firstname FROM users WHERE firstname = :firstname");
+                $check_name->bindParam(":firstname", $firstname);
+                $check_name->execute();
+                $row = $check_name->fetch(PDO::FETCH_ASSOC);
+
+                if ($row['firstname'] == $firstname) {
+                    $_SESSION['warning'] = "มีชื่อจริงนี้ในระบบแล้ว";
                     header("location: index.php");
-                } else {
-                    $stmt = $condb->prepare("INSERT INTO users (firstname, lastname, email, password, urole) VALUES (:firstname, :lastname, :email, :password, :urole)");
+                } else if (!isset($_SESSION['error'])) {
+                    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+                    $stmt = $condb->prepare("INSERT INTO users(firstname, lastname, email, password, urole) 
+                                            VALUES(:firstname, :lastname, :email, :password, :urole)");
                     $stmt->bindParam(":firstname", $firstname);
                     $stmt->bindParam(":lastname", $lastname);
                     $stmt->bindParam(":email", $email);
-                    $stmt->bindParam(":password", $password_hash);
+                    $stmt->bindParam(":password", $passwordHash);
                     $stmt->bindParam(":urole", $urole);
                     $stmt->execute();
-
-                    $_SESSION['success'] = 'สมัครสมาชิกสำเร็จ';
-                    header("location: signin.php");
+                    $_SESSION['success'] = "สมัครสมาชิกเรียบร้อยแล้ว! <a href='signin.php' class='alert-link'>คลิ๊กที่นี่</a> เพื่อเข้าสู่ระบบ";
+                    header("location: index.php");
+                } else {
+                    $_SESSION['error'] = "มีบางอย่างผิดพลาด";
+                    header("location: index.php");
                 }
             } catch(PDOException $e) {
                 echo $e->getMessage();
