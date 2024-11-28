593736d3f4d129a8b4a24b60aad7de931c4057d5
chinnawat26755 <chinnawat.aoum@bumail.net>
2024-11-28 19:20:48 +0700

    Update member_content.php

--- admin/member_content.php
+++ admin/member_content.php
@@ -34,7 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 title: "Email sent successfully",
                 type: "success"
             }, function() {
-                window.location.href = "member_list.php";
+                window.location.href = "/admin/index.php";
             });
         </script>';
     } catch (PDOException $e) {
